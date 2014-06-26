55df9b521df29bb8f22215eed1f50dec214aedaf
OutSider <OutSider@OutSider-PC>
2014-06-26 12:43:04 +0900

    ProductController Modify ver 0.1

--- SOS_gomingomin/src/main/java/org/sos/controller/ProductController.java
+++ SOS_gomingomin/src/main/java/org/sos/controller/ProductController.java
@@ -1,9 +1,15 @@
 package org.sos.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
+import org.sos.service.CategoryService;
 import org.sos.service.ProductService;
+import org.sos.vo.CategoryVO;
 import org.sos.vo.ProductVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +23,9 @@ public class ProductController {
 	@Inject
 	ProductService productService;
 	
+	@Inject
+	CategoryService categoryService;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView getProductManagementPage(String pageNo){
 		
@@ -26,9 +35,27 @@ public class ProductController {
 		}
 		
 		ModelAndView mv = new ModelAndView();
+		CategoryVO categoryVO = null;
+		List<CategoryVO> categoryList = null;
+		Map<Integer, String> parentCategoryNameMap = new HashMap<Integer, String>();
+		Map<Integer, String> childCategoryNameMap = new HashMap<Integer, String>();
 		
 		try {
 			mv.addObject("productList", productService.readProductList(Integer.parseInt(pageNo)));
+			categoryList = categoryService.readAllCategory();
+			
+			for(int i=0; i<categoryList.size(); i++){
+				categoryVO = categoryList.get(i);
+				if(categoryVO.getCategory_depth().equals("0")){
+					parentCategoryNameMap.put(categoryVO.getCategory_id(), categoryVO.getCategory_name());
+				}else{
+					childCategoryNameMap.put(categoryVO.getCategory_id(), categoryVO.getCategory_name());
+				}
+			}
+			
+			mv.addObject("parentCategoryNameMap", parentCategoryNameMap);
+			mv.addObject("childCategoryNameMap", childCategoryNameMap);
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
